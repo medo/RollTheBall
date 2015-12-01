b55b4a421c0cc27ccb33c630f720a0963e4b7644
Almgohar <Almgohar@Almgohar.local>
2015-12-02 00:06:40 +0200

    Fix bugs

--- src/eg/edu/guc/rolltheball/logic/converters/Unificiation.java
+++ src/eg/edu/guc/rolltheball/logic/converters/Unificiation.java
@@ -85,6 +85,7 @@ public class Unificiation {
 
 				if ((r instanceof Function) && (s instanceof Function)
 						&& !((Function) r).toString().equals(((Function) s).toString())) {
+					if(substFunc(r, s, theta) != null)
 					theta = substFunc(r, s, theta);
 					return unify_helper(subst(theta, p), subst(theta, q), theta);
 
@@ -133,7 +134,7 @@ public class Unificiation {
 	public static Predicate subst(HashMap<Variable, Term> theta, Predicate x) {
 		for (Term term : x.terms) {
 			if (term instanceof Variable) {
-				if (theta.get((Variable) term) != null) {
+				if (theta != null && theta.get((Variable) term) != null) {
 					x.terms.set(x.terms.indexOf(term), theta.get((Variable) term));
 				}
 			} else {
@@ -149,7 +150,7 @@ public class Unificiation {
 	public static Function sub(HashMap<Variable, Term> theta, Function f) {
 		for (Term fTerm : f.terms) {
 			if (fTerm instanceof Variable) {
-				if (theta.get((Variable) fTerm) != null) {
+				if (theta != null && theta.get((Variable) fTerm) != null) {
 					f.terms.set(f.terms.indexOf(fTerm), theta.get((Variable) fTerm));
 				}
 				return f;
@@ -165,17 +166,17 @@ public class Unificiation {
 
 	public static void main(String[] args) {
 		// parents(x, father(x), mother(Bill)) & parents(Bill, father(Bill), y)
-		 Variable x = new Variable("x");
-		 Function father1 = new Function("father", x);
-		 Constant bill = new Constant("Bill");
-		 Function mother1 = new Function("mother", bill);
-		
-		 Predicate p = new Predicate("parents", new Term[] { x, father1, mother1
-		 });
-		
-		 Variable y = new Variable("y");
-		 Function father = new Function("father", bill);
-		 Predicate q = new Predicate("parents", new Term[] { bill, father, y });
+//		 Variable x = new Variable("x");
+//		 Function father1 = new Function("father", x);
+//		 Constant bill = new Constant("Bill");
+//		 Function mother1 = new Function("mother", bill);
+//		
+//		 Predicate p = new Predicate("parents", new Term[] { x, father1, mother1
+//		 });
+//		
+//		 Variable y = new Variable("y");
+//		 Function father = new Function("father", bill);
+//		 Predicate q = new Predicate("parents", new Term[] { bill, father, y });
 
 		// P(x; g(x); g(f(a))) and P(f(u); v; v)
 
@@ -184,37 +185,41 @@ public class Unificiation {
 		// Constant a = new Constant("a");
 		// Function fOfA = new Function("f", a);
 		// Function gOfF = new Function("g", fOfA);
-		// Predict p = new Predict("p", new Term[] { x, gOfX, gOfF });
+		// Predicate p = new Predicate("p", new Term[] { x, gOfX, gOfF });
 		//
 		// Variable u = new Variable("u");
 		// Variable v = new Variable("v");
 		// Function fOfU = new Function("f", u);
-		// Predict q = new Predict("p", new Term[] { fOfU, v, v });
+		// Predicate q = new Predicate("p", new Term[] { fOfU, v, v });
 
 		// P(a; y; f(y)) and P(z; z; u)
 
-//		Constant a = new Constant("a");
-//		Variable y = new Variable("y");
-//		Function f = new Function("f", y);
-//		Predict p = new Predict("p", new Term[] { a, y, f });
-//
-//		Variable z = new Variable("z");
-//		Variable u = new Variable("u");
-//		Predict q = new Predict("p", new Term[] { z, z, u });
+		Constant a = new Constant("a");
+		Variable y = new Variable("y");
+		Function f = new Function("f", y);
+		Predicate p = new Predicate("p", new Term[] { a, y, f });
+
+		Variable z = new Variable("z");
+		Variable u = new Variable("u");
+		Predicate q = new Predicate("p", new Term[] { z, z, u });
 
 		// f(x; g(x); x) and f(g(u); g(g(z)); z)
 
-		// Variable x = new Variable("x");
-		// Function gOfX = new Function("g", x);
-		// Predict p = new Predict("f", new Term[] { x, gOfX, x });
-		//
-		// Variable u = new Variable("u");
-		// Variable z = new Variable("z");
-		// Function gOfZ = new Function("g", z);
-		// Function gOfG = new Function("g", gOfZ);
-		// Function gOfU = new Function("g", u);
-		// Predict q = new Predict("f", new Term[] { gOfU, gOfG, z });
-		System.err.println(unify_helper(p, q, new HashMap<Variable, Term>()).toString());
+//		 Variable x = new Variable("x");
+//		 Function gOfX = new Function("g", x);
+//		 Predicate p = new Predicate("f", new Term[] { x, gOfX, x });
+//		
+//		 Variable u = new Variable("u");
+//		 Variable z = new Variable("z");
+//		 Function gOfZ = new Function("g", z);
+//		 Function gOfG = new Function("g", gOfZ);
+//		 Function gOfU = new Function("g", u);
+//		 Predicate q = new Predicate("f", new Term[] { gOfU, gOfG, z });
+		HashMap<Variable, Term> answer = unify(p, q);
+		if(answer != null)
+			System.err.println(answer.toString());
+		else 
+			System.err.println("False");
 	}
 
 }
