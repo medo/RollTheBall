bb0ed1dcc55d4fddede486d5c4711dc9b3b6c903
MohamedBassem <medoox240@gmail.com>
2015-12-01 20:08:59 +0200

    Adding clause and literal

---
+++ src/eg/edu/guc/rolltheball/logic/grammer/Clause.java
@@ -0,0 +1,7 @@
+package eg.edu.guc.rolltheball.logic.grammer;
+
+import java.util.ArrayList;
+
+public class Clause extends ArrayList<Literal>{
+
+}
---
+++ src/eg/edu/guc/rolltheball/logic/grammer/Literal.java
@@ -0,0 +1,6 @@
+package eg.edu.guc.rolltheball.logic.grammer;
+
+public class Literal {
+    public Predict predicate;
+    public boolean negated;
+}
